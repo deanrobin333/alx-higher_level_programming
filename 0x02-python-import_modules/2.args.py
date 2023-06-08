d8f0d221f0f5c1c85f79744218c0fd356a018b39
deanrobin333 <deanrobin777@gmail.com>
2023-06-08 23:34:16 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2.args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    tally = len(sys.argv) - 1
+    if tally == 0:
+        print("0 arguments.")
+    elif tally == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(tally))
+    for i in range(tally):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
