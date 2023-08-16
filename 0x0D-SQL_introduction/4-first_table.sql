f9d7a285b884e667dd0a60d675434ff08fabc1c1
deanrobin333 <deanrobin777@gmail.com>
2023-08-16 03:14:20 +0300

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- creates a table called first_table
+-- first_table description:
+--  id INT
+--  name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table(
+        id INT,
+        name VARCHAR(256)
+);
