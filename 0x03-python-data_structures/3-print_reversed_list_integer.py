6e1be2500d437ffe0903175e5cfb361bb68885b2
deanrobin333 <deanrobin777@gmail.com>
2024-02-13 19:47:25 +0300

    3-print_reversed_list_integer.py pycodestyle

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if isinstance(my_list, list) == False:
+    if not isinstance(my_list, list):
         return
     length = len(my_list)
     for i in range(length - 1, -1, -1):
