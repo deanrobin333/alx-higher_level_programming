79124ec2b4e0fe225d022af28fd0454292e54a42
deanrobin333 <deanrobin777@gmail.com>
2024-02-13 21:40:52 +0300

    7-add_tuple.py changed condition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = (
             (int(tuple_a[0]) + (int(tuple_b[0]) if len(tuple_b) >= 1 else 0)),
-            (int(tuple_a[1]) + (int(tuple_b[1]) if len(tuple_b) == 2 else 0))
+            (int(tuple_a[1]) + (int(tuple_b[1]) if len(tuple_b) >= 2 else 0))
             )
     return new_tuple
