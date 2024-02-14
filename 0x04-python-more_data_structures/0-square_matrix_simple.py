3ede76f5273a5dd8042cb937b2ca9c5c509e9d96
deanrobin333 <deanrobin777@gmail.com>
2024-02-14 23:42:10 +0300

    0-square_matrix_simple.py indentation is vital

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,4 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in range(len(matrix)):
         new_matrix.append(list(map(lambda x: x * x, matrix[i])))
-        return new_matrix
+    return new_matrix
