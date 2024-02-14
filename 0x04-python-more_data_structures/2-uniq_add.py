b7c53c7e0496474dc9619fb5ecc8fc8c458f36a6
deanrobin333 <deanrobin777@gmail.com>
2024-02-15 01:09:34 +0300

    2-uniq_add.py used indexing

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sorted_list = sorted(my_list)
-    diff_value = sorted_list[0]
-    result = 0
+    result = sorted_list[0]
 
     for i in range(1, len(sorted_list)):
-        result += diff_value if diff_value != sorted_list[i] else 0
-        diff_value = sorted_list[i]
-
-    if sorted_list[-2] != diff_value:
-        result += diff_value
+        if sorted_list[i] != sorted_list[i - 1]:
+            result += sorted_list[i]
 
     return result
