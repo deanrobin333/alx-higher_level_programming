708136ec11443749b4f36da82abc9573dacc5c5b
deanrobin333 <deanrobin777@gmail.com>
2024-02-15 01:11:51 +0300

    2-uniq_add.py empty list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     sorted_list = sorted(my_list)
     if len(sorted_list) == 0:
-        return
+        return 0
     result = sorted_list[0]
 
     for i in range(1, len(sorted_list)):
