770b9a42519a7fb0f156c847112b0ddd1a7772c7
deanrobin333 <deanrobin777@gmail.com>
2024-02-15 20:47:11 +0300

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return
+    list_keys = []
+    new_dict = dict(a_dictionary)
+    for key, val in a_dictionary.items():
+        if val == value:
+            list_keys.append(key)
+
+    for i in list_keys:
+        if i in a_dictionary:
+            del a_dictionary[i]
+
+    return (a_dictionary)
