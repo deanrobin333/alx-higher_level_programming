b797487c6414f86542ada5e5be72f3b39740c04d
deanrobin333 <deanrobin777@gmail.com>
2024-02-15 20:05:44 +0300

    100-weight_average.py check for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     weight_total = 0
     score_total = 0
     score_list = list(map(lambda x: x[0] * x[1], my_list))
