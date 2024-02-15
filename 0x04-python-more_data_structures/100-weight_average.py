b117a6cd2de572b8cc3fe943aa0b21550883f35a
deanrobin333 <deanrobin777@gmail.com>
2024-02-15 20:02:46 +0300

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    weight_total = 0
+    score_total = 0
+    score_list = list(map(lambda x: x[0] * x[1], my_list))
+
+    for i in range(len(my_list)):
+        weight_total += my_list[i][1]
+    for i in score_list:
+        score_total += i
+
+    result = score_total / weight_total
+
+    return result
