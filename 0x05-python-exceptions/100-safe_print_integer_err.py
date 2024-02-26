0b5a1ab6f3ac564dda232e2116997560cbc44570
deanrobin333 <deanrobin777@gmail.com>
2024-02-26 20:14:32 +0300

    100-safe_print_integer_err.py changed position of return, and used

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e.args[0]), file=sys.stderr)
         return False
-        print("Excetion: {}".format(e), file=sys.stderr)
