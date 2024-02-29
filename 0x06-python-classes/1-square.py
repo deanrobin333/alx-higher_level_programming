e7a453bb3185d1c9ea71ae6dd4129ee98d6e65a4
deanrobin333 <deanrobin777@gmail.com>
2024-02-29 23:36:27 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Defines a square base on 0-square.py'''
+
+
+class Square:
+    '''Represents a square'''
+
+    def __init__(self, size):
+        '''
+        initiates a square
+
+        Args:
+            size (int): size of the new square.
+        '''
+
+        self.__size = size
