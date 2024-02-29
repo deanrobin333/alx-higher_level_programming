29e899bb799aeed1e7b884495d37329185c4efc7
deanrobin333 <deanrobin777@gmail.com>
2024-03-01 02:35:38 +0300

    6-square.py - sumple syntax error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,7 +70,7 @@ class Square:
            ):
             raise TypeError("position must be a tuple of 2 positive integers")
 
-        self.__position(value)
+        self.__position = value
 
     def area(self):
         '''
