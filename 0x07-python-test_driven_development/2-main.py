e2a898d779856cfa21aa38cae7f34e848e385d01
deanrobin333 <deanrobin777@gmail.com>
2024-03-09 18:08:24 +0300

    3-say_my_name.py, tests/3-say_my_name.txt

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -7,18 +7,3 @@ matrix = [
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
-
-matrix = [[3, 7, 9], [5, 9, 11]]
-print(matrix_divided(matrix, 3))
-
-matrix = [[1, 2, 3], [4, 5, 6]]
-print(matrix_divided(matrix, 3.3))
-print(matrix_divided(matrix, -3.3))
-
-matrix = [[3, -2.2, 9], [-6.6, 7.33, 8]]
-print(matrix_divided(matrix, 3))
-
-print()
-matrix = [[1, 2, 3], [4, 5, 6]]
-print(matrix_divided(matrix, 1))
-
