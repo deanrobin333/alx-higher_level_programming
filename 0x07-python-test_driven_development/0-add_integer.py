f6a17f017b261450cd002478fb22931ca8f9d039
deanrobin333 <deanrobin777@gmail.com>
2024-03-09 06:10:32 +0300

    0-add_integer.py, tests/0-add_integer.txt

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    '''
+    function that adds integers
+
+    Converts floating points to integers first, before addition.
+    if a or b is not an integer or a float it raises an exception
+    '''
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    elif (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+
+        return a + b
