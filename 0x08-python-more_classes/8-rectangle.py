1fe64203890f0b3d5837a05ac4b12a3495d8296e
deanrobin333 <deanrobin777@gmail.com>
2024-03-24 02:32:27 +0300

    8-rectangle.py added comments for bigger_or_equal

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -83,6 +83,12 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        '''Return the Rectangle with the biggest area
+
+        Parameters:
+            rect_1 (Rectangle): The 1st Rectangle
+            rect_2 (Rectangle): The 2nd Rectangle
+        '''
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
