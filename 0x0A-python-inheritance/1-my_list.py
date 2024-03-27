0c495ae93ed2c4da9a0e7f687521a4bff70b44dc
deanrobin333 <deanrobin777@gmail.com>
2024-03-28 01:30:10 +0300

    1-my_list.py - fine tuning

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+# 1-my_list.py
 '''Defines a class MyList that inherits from List'''
 
 
 class MyList(list):
-    '''class inheriting from list'''
+    '''class inheriting from list
+
+    Parameter:
+        list (list): list to sort
+    '''
 
     def print_sorted(self):
         '''Prints a list in ascending order'''
