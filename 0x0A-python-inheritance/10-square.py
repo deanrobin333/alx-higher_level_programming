49318d672162e0847ad17856578bb926c2811503
deanrobin333 <deanrobin777@gmail.com>
2024-03-28 21:37:41 +0300

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 10-square.py
+
+'''Defines a class ``Square``
+that inherits from `Rectangle` ``9-rectangle.py)``
+
+Attributes:
+    width (int): width of the rectangle.
+    height (int): height of the rectangle.
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' Defines a Square class'''
+
+    def __init__(self, size):
+        '''Creates new instances of class Square.
+
+        Args:
+            size (int): size of 1 side of square.
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
