fa0a57f294fcce6dfe72cd1c6684c69bfa7f2baa
deanrobin333 <deanrobin777@gmail.com>
2024-04-04 04:21:32 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''defines a function ``append_write``'''
+
+
+def append_write(filename="", text=""):
+    '''function that appends a string to a text file
+    and returns the number of characters added
+
+    Args:
+        filename (str) : name of file
+        text (str) : string to append to file
+
+    Returns:
+        (int) : number of characters written
+    '''
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
