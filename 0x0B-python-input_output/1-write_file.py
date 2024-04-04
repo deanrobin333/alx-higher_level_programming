221169ebc5210bb5ee726594b4fa0b1120c672c8
deanrobin333 <deanrobin777@gmail.com>
2024-04-04 04:15:12 +0300

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''defines a function ``write_file()``'''
+
+
+def write_file(filename="", text=""):
+    '''function that writes a string to a text file
+    and returns the number of characters written
+
+    Args:
+        filename (str) : name of file
+        text (str) : string to write to file
+
+    Returns:
+        (int) : number of characters written
+    '''
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
+
