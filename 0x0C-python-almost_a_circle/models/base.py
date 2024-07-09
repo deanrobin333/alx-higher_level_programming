3f675077a3afbd05503ae9bdf1d5e9e709727465
deanrobin333 <deanrobin777@gmail.com>
2024-07-09 21:44:34 +0300

    base.py - def to_json_string(list_dictionaries) - changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,8 +35,6 @@ class Base:
         Returns:
             (str): json dictionary representation
         '''
-
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
-
         return json.dumps(list_dictionaries)
