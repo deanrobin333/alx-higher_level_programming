01f21b6b8ccde2dcff77073ddb98c3c8492ae3fb
deanrobin333 <deanrobin777@gmail.com>
2024-07-09 19:44:37 +0300

    models/rectangle.py dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -165,3 +165,11 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        '''returns the dictionary representation of a rectangle'''
+        new_dict = {
+            'id': self.id, 'width': self.width, 'height': self.height,
+            'x': self.x, 'y': self.y
+        }
+        return new_dict
