ba750ac6ed13755ce0f4ed25685548ccf45998be
deanrobin333 <deanrobin777@gmail.com>
2024-07-09 23:28:43 +0300

    base.py - def save_to_file(cls, list_objs)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,29 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): Instances who inherits of Base - eg
+                list of Rectangle or list of Square instances
+        '''
+        file_name = f"{cls.__name__}.json"  # make output file from class name
+
+        '''List that will hold dictionary representation of all instances
+        of each class
+        '''
+        dic_list = []
+
+        if list_objs:  # if list_objs is not empty
+            for i in range(len(list_objs)):  # through every object instance
+                '''convert every object instance to it's dictionary
+                representation, then append it to the list
+                '''
+                dic_list.append(list_objs[i].to_dictionary())
+
+        with open(file_name, 'w') as f:
+            '''Serialize the dictionary to json string, then write to file'''
+            f.write(cls.to_json_string(dic_list))
