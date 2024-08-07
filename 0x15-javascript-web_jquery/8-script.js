24298f4819abaa9d2e31877052032dac2d1df622
deanrobin333 <deanrobin777@gmail.com>
2024-08-08 01:18:17 +0300

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+// 8-script.js
+/* fetches and lists the title for all movies by using this URL:
+ * https://swapi-api.hbtn.io/api/films/?format=json
+ */
+const $ = window.$;
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  console.log(data);
+  data.results.forEach(film => {
+    $('UL#list_movies').append(film.title);
+  });
+});
