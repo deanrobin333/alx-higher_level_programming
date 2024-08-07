3d177c21e28e12335faee28db996a85e680777f7
deanrobin333 <deanrobin777@gmail.com>
2024-08-08 01:15:52 +0300

    7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+// 7-script.js
+/* fetches the character name from this URL:
+ * https://swapi-api.hbtn.io/api/people/5/?format=json
+ */
+const $ = window.$;
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('#character').append(data.name);
+});
