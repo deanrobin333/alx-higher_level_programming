fcc81e895f32e1c9cc91bacf23d938c042886dba
deanrobin333 <deanrobin777@gmail.com>
2024-08-08 00:50:38 +0300

    2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+// 2-script.js
+// updates the text color of the <header> element
+// to red (#FF0000) when the user clicks on the tag DIV#red_header:
+const $ = window.$;
+$('#red_header').bind('click', function () {
+  $('header').css({ color: '#FF0000' });
+});
